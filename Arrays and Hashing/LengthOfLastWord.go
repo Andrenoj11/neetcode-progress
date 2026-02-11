7135fb7243edd0cd24a34bca219a05296af61bf8
Andreno Juanda <andrenojuanda94@gmail.com>
2026-02-11 22:28:54 +0700

    finish length of last word

---
+++ Arrays and Hashing/LengthOfLastWord.go
@@ -0,0 +1,14 @@
+package main
+
+func lengthOfLastWord(s string) int {
+	count := 0
+
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] != ' ' {
+			count++
+		} else if count > 0 {
+			break
+		}
+	}
+	return count
+}
