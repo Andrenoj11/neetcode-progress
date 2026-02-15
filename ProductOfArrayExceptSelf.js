0c23ef8e3e76aef7bbf785b34d644a3eec5e977c
Andreno Juanda <andrenojuanda94@gmail.com>
2026-02-15 21:27:11 +0700

    update products of array excet self

--- ProductOfArrayExceptSelf.js
+++ ProductOfArrayExceptSelf.js
@@ -14,23 +14,38 @@ class Solution {
         //     output.push(product)
         // }
         // return output
-        const n = nums.length;
-    const output = new Array(n);
+    //     const n = nums.length;
+    // const output = new Array(n);
 
-    // 1) prefix products (produk semua elemen di kiri i)
-    let prefix = 1;
-    for (let i = 0; i < n; i++) {
-      output[i] = prefix;
-      prefix *= nums[i];
+    // // 1) prefix products (produk semua elemen di kiri i)
+    // let prefix = 1;
+    // for (let i = 0; i < n; i++) {
+    //   output[i] = prefix;
+    //   prefix *= nums[i];
+    // }
+
+    // // 2) suffix products (produk semua elemen di kanan i), lalu kalikan ke output
+    // let suffix = 1;
+    // for (let i = n - 1; i >= 0; i--) {
+    //   output[i] *= suffix;
+    //   suffix *= nums[i];
+    // }
+
+    // return output;
+
+    const output = Array(nums.length).fill(1)
+    let prefix = 1
+    for (let i = 0; i < nums.length; i++){
+        output[i] = prefix
+        prefix *= nums[i]
     }
-
-    // 2) suffix products (produk semua elemen di kanan i), lalu kalikan ke output
-    let suffix = 1;
-    for (let i = n - 1; i >= 0; i--) {
-      output[i] *= suffix;
-      suffix *= nums[i];
+    console.log(output)
+    let postfix = 1
+    for (let i = nums.length-1; i >= 0; i--){
+        output[i] *= postfix
+        postfix *= nums[i]
     }
-
-    return output;
+    console.log(output)
+    return output
     }
 }
