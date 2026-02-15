7aa50b6f8551cf46208b34697e5e3287a4e44f83
Andreno Juanda <andrenojuanda94@gmail.com>
2026-02-15 20:24:40 +0700

    finish product of array except self

---
+++ ProductOfArrayExceptSelf.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        // const output = []
+        // for (let i = 0; i < nums.length; i++){
+        //     let product = 1
+        //     for (let j = 0; j < nums.length;j++){
+        //         if(i === j) continue
+        //         product *= nums[j]
+        //     }
+        //     output.push(product)
+        // }
+        // return output
+        const n = nums.length;
+    const output = new Array(n);
+
+    // 1) prefix products (produk semua elemen di kiri i)
+    let prefix = 1;
+    for (let i = 0; i < n; i++) {
+      output[i] = prefix;
+      prefix *= nums[i];
+    }
+
+    // 2) suffix products (produk semua elemen di kanan i), lalu kalikan ke output
+    let suffix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+      output[i] *= suffix;
+      suffix *= nums[i];
+    }
+
+    return output;
+    }
+}
